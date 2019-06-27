6826eb014ad691d0a44bfc563d6494c8293fe793
Demfez <demfezeldar@gmail.com>
2019-06-27 14:18:30 +0300

    simple without serializer

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,8 +6,6 @@ namespace App\Controller;
 // ...
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\SerializerInterface;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -52,10 +50,9 @@ class ProductController extends AbstractController
     /**
      * @Route("/product/{id<\d+>}", name="product_show")
      * @param $id
-     * @param SerializerInterface $serializer
      * @return string
      */
-    public function show($id, SerializerInterface $serializer)
+    public function show($id)
     {
         $product = $this->getDoctrine()
             ->getRepository(Product::class)
@@ -67,8 +64,9 @@ class ProductController extends AbstractController
             );
         }
 
-        $json_product = $serializer->serialize($product, 'json');
-        return new Response($json_product);
+//        $json_product = $serializer->serialize($product, 'json');
+//        return new Response($json_product);
+        return $this->json($product);
 
         // or render a template
         // in the template, print things with {{ product.name }}
